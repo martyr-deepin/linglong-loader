89966984ef435c36be4610db4e2dbed07e45d3e3
liujianqiang <liujianqiang@uniontech.com>
2022-06-18 15:39:47 +0800

    fix: 添加linglong-loader测试代码
    添加linglong-loader测试代码.
    
    Log:
    Change-Id: Ie4a2a9953bacb4645958d6a34e50a66a6c22d67d

--- tests/demo_test.cpp
+++ tests/demo_test.cpp
@@ -1,27 +1,46 @@
 /*
- * Copyright (c) 2019-2021. Uniontech Software Ltd. All rights reserved.
+ * Copyright (c) 2022. Uniontech Software Ltd. All rights reserved.
  *
- * Author:     Heysion <heysoin@deepin.com>
+ * Author:     liujianqiang <liujianqiang@uniontech.com>
  *
- * Maintainer: Heysion <heysion@deepin.com>
+ * Maintainer: liujianqiang <liujianqiang@uniontech.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#include <iostream>
 
 #include <gtest/gtest.h>
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
+#include <QProcess>
 
-TEST(DemoTest, demo01) {
-    EXPECT_EQ(true, true);
+namespace linglong::squashfs {
+int MountFile(const std::string &path, unsigned long offset, const std::string &mount_point);
+}
+
+namespace linglong::elf {
+long GetELFSize(const std::string &path);
+}
+
+TEST(DemoTest, demo01)
+{
+    QFileInfo fs("../../tests/data/org.deepin.demo_1.2.1_x86_64.uab");
+    EXPECT_EQ(fs.exists(), true);
+    auto size = linglong::elf::GetELFSize("../../tests/data/org.deepin.demo_1.2.1_x86_64.uab");
+    std::cout << size << std::endl;
+    EXPECT_EQ(size, 2326904);
+    std::string mountPoint("/tmp/uab_test");
+    QDir("/tmp/uab_test").mkpath(".");
+    //fuse mount uab
+    auto result = linglong::squashfs::MountFile("../../tests/data/org.deepin.demo_1.2.1_x86_64.uab", size, mountPoint);
+    //fuse umount uab
+    QProcess process;
+    process.setProgram("fusermount3");
+    process.setStandardOutputFile("/dev/null");
+    process.setStandardErrorFile("/dev/null");
+    process.setArguments({"-u",QString("/tmp/uab_test")});
+    process.startDetached();
+    EXPECT_EQ(result,0);
 }
